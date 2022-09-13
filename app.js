8f83ee0c359c98992c254a6899c9d89ff70a7373
Jacob Pixler <drivrartist@gmail.com>
2022-09-12 22:47:59 -0500

    Added Inspirational Quotes API

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
-const inputBar = $('#inputBar');
-const inputValue = $('#inputBar').value;
-const introPrompt = $('#introPrompt');
-const submit = $('submitBtn');
+$(document).ready(function() {
+    const inputBar = $('#inputBar');
+    const inputValue = $('#inputBar').value;
+    const introPrompt = $('#introPrompt');
+    const submit = $('#submitBtn');
+    const quote = $('#quote');
+    const authors = $('#author');
 
-inputBar.click(() => {
-    introPrompt.slideToggle(2000);
-});
+    submit.click((e) => {
+        e.preventDefault();
+        introPrompt.slideUp(2000);
+        quote.removeClass('noDisplay');
+        fetch('https://type.fit/api/quotes')
+        .then((response) => response.json())
+        .then((data) => {
+            quote[0].innerHTML = `"` + data[Math.floor(Math.random() * data.length)].text + `"`
+            //authors.innerHTML = `-` + data[Math.floor(Math.random() * data.length)].author
+        })
+        .catch((err) => console.log(err))
+    });
 
+    inputBar.click(() => {
+        introPrompt.slideDown(2000);
+        quote.addClass('noDisplay');
+    });
+})
