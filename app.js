0251a8dcf54a37e1199383002032c0844ed208a6
Jacob Pixler <drivrartist@gmail.com>
2022-09-13 17:43:48 -0500

    Fixed Quote Fade In

--- app.js
+++ app.js
@@ -3,24 +3,32 @@ $(document).ready(function() {
     const inputValue = $('#inputBar').value;
     const introPrompt = $('#introPrompt');
     const submit = $('#submitBtn');
-    const quote = $('#quote');
+    const quoteSpace = $('#quote');
     const authors = $('#author');
+    
+
+    function generateQuote() {
+        fetch('https://type.fit/api/quotes')
+        .then((response) => response.json())
+        .then((data) => {
+            quoteSpace[0].innerHTML = `"` + data[Math.floor(Math.random() * data.length)].text + `"`
+            //authors.innerHTML = `-` + data[Math.floor(Math.random() * data.length)].author
+        })
+        .catch((err) => console.log(err));
+    }
 
     submit.click((e) => {
         e.preventDefault();
         introPrompt.slideUp(2000);
-        quote.removeClass('noDisplay');
-        fetch('https://type.fit/api/quotes')
-        .then((response) => response.json())
-        .then((data) => {
-            quote[0].innerHTML = `"` + data[Math.floor(Math.random() * data.length)].text + `"`
-            //authors.innerHTML = `-` + data[Math.floor(Math.random() * data.length)].author
-        })
-        .catch((err) => console.log(err))
+        quoteSpace.removeClass('noDisplay');
+        inputBar.fadeOut(2000);
+        submit.fadeOut(2000);
+        generateQuote();
+        quoteSpace.addClass('fadeIn')
     });
 
     inputBar.click(() => {
         introPrompt.slideDown(2000);
-        quote.addClass('noDisplay');
+        quoteSpace.addClass('noDisplay');
     });
 })
